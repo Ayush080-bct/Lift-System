842f1fd7b153aa7ca3b2be6577251f3205b6244e
Ayush sahu <shahaayush526@gmail.com>
2026-05-13 23:59:30 +0545

    lift_status

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from model.models import  Lift,Request,Log
+from repository.repository import LiftRepository
 app=FastAPI()
-
+repo=LiftRepository()
 @app.get("/")
 def root():
     return {"Message":"Lift System API running"}
+@app.get('/lift_Status/{lift_id}')
+def lift_status(lift_id:int):
+    result=repo.get_lift(lift_id)
+    if result:
+        return {'lift_id':result[0],'floor':result[1]}
+    return {'error':"lift not found"}
 
 
--- backend/repository/repository.py
+++ backend/repository/repository.py
@@ -4,5 +4,10 @@ from model.databases import conn
 from model.models import Lift,Log,Request
 class LiftRepository:
     def get_lift(self,lift_id:int):
+        """fetch by id"""
         cursor=conn.cursor()
-        cursor.execute('SELECT * FROM lifts WHERE lift_id=%s',)
+        cursor.execute('SELECT * FROM lifts WHERE lift_id=%s',(lift_id,))
+        result=cursor.fetchone()
+        cursor.close()
+        return result
+    
