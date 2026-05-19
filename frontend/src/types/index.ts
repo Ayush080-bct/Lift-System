63fd962cd7e80bd0c468b5ff7725ac2056ab625c
Ayush sahu <shahaayush526@gmail.com>
2026-05-19 23:04:36 +0545

    simple changes

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,3 +1,6 @@
+//an interface is a way to define the shape of an object — 
+// basically a contract that says: “Any object of this type must have these properties, with these types.”
+//It doesn’t generate code at runtime; it’s purely for type checking during development.
 export interface Lift{
     lift_id:number;
     current_floor:number;
