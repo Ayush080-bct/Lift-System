c2ec4fe5e1dd54ba7099405af1334fe0ec4de08b
Ayush sahu <shahaayush526@gmail.com>
2026-05-19 23:29:05 +0545

    changes in functiond declaration

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,15 +1,13 @@
 import type { Lift } from "../types";
 const API_URL="http://localhost:8000";
-export const apiService={
-    getAlllifts: async ():Promise<Lift[]>=>{
-        const response=await fetch(`${API_URL}/lifts`)
-        const data=await response.json();
-        return data.lifts || [];
-    },
-    createRequest: async (floor:number)=>{
-        const response=await fetch(`${API_URL}/requests?floor=${floor}`,{
-            method:"POST",
-        });
-        return response.json();
-    },
-};
+export async function getAllLifts():Promise<Lift[]> {
+    const response=await fetch(`${API_URL}/lifts`);
+    const data=await response.json();
+    return data.lifts || [];
+}
+export async function createRequest(floor:number){
+    const response=await fetch(`${API_URL}/requests?floor=${floor}`,{
+        method:"POST",
+    });
+    return response.json();
+}
